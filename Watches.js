84ffa9671d3873f717de7c9ce4cf5f8353268b22
QahramanTaha <qahramantaha@atu.ie>
2024-05-06 19:25:37 +0100

    updated

--- Watches.js
+++ Watches.js
@@ -1,6 +1,6 @@
 
 
-/*Q.T*/function openCart() {
+/*Qahraman Taha https://www.w3schools.com/jsref/met_win_open.asp opens cart.html when event (onclick) is clicked */function openCart() {
 
     window.open("Cart.html");
 
